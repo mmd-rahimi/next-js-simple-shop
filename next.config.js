79d58c54effdb935ed8ce764bdaf7452ffbe89f8
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-08-02 13:12:31 -0700

    a simple shop with next.js, tailwind completed. here we have shop with some products, cart and product detail page. state management with context

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // اگر خواستی بعداً flagهای experimental بذاری:
-  // experimental: { appDir: true },
 };
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import ProductList from "@/components/ProductList";
 export default async function Home() {
 
   const res = await fetch("https://fakestoreapi.com/Products")
-  const products = await res.json()  
-  console.log(products);
-  
+  const products = await res.json()    
 
   return(
     <div className="flex items-center justify-center">
